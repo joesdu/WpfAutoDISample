0c44eba86686ea9cfe99124c57069bc5d7c509f3
Joes <dygood@outlook.com>
2024-06-27 15:10:42 +0800

    添加项目文件。

---
+++ WpfAutoDISample/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using EasilyNET.AutoDependencyInjection.Core.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using WpfAutoDISample.ViewModels;
+
+namespace WpfAutoDISample;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+[DependencyInjection(ServiceLifetime.Singleton, AddSelf = true)]
+public partial class MainWindow
+{
+    private readonly MainWindowModel _model;
+
+    /// <inheritdoc />
+    public MainWindow(MainWindowModel mwm)
+    {
+        InitializeComponent();
+        _model = mwm;
+        DataContext = mwm;
+    }
+
+    private async void OnBtnClick(object sender, RoutedEventArgs e)
+    {
+        _model.Message = "Clicked";
+        await Task.CompletedTask;
+    }
+}
