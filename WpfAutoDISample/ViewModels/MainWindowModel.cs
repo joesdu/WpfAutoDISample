fe90aaa733475cf3fb634a2509e186dd6e487e13
Joes <dygood@outlook.com>
2024-06-28 11:54:32 +0800

    fix: 更新依赖

--- WpfAutoDISample/ViewModels/MainWindowModel.cs
+++ WpfAutoDISample/ViewModels/MainWindowModel.cs
@@ -3,13 +3,12 @@ using EasilyNET.AutoDependencyInjection.Core.Attributes;
 using Microsoft.Extensions.DependencyInjection;
 
 // ReSharper disable PartialTypeWithSinglePart
-
 // ReSharper disable ClassNeverInstantiated.Global
 
 namespace WpfAutoDISample.ViewModels;
 
 /// <inheritdoc cref="ObservableObject" />
-[DependencyInjection(ServiceLifetime.Singleton, AddSelf = true)]
+[DependencyInjection(ServiceLifetime.Singleton, AddSelf = true, SelfOnly = true)]
 public partial class MainWindowModel : ObservableObject
 {
     /// <summary>
@@ -18,3 +17,19 @@ public partial class MainWindowModel : ObservableObject
     [ObservableProperty]
     private string message = "Hello WPF!";
 }
+
+// 使用接口注入的例子
+//public partial class MainWindowModel : ObservableObject, ISingletonDependency
+//{
+//    /// <summary>
+//    /// Message
+//    /// </summary>
+//    [ObservableProperty]
+//    private string message = "Hello WPF!";
+
+//    // ReSharper disable once UnusedMember.Global
+//    public static bool? DependencyInjectionSelf => true;
+
+//    // ReSharper disable once UnusedMember.Global
+//    public static bool? DependencyInjectionSelfOnly => true;
+//}
